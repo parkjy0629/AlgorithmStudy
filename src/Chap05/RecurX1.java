44d2bea49a59c0d1337bcfbadda2b5b9f1e8c445
PJY <parkjy0629@gmail.com>
2021-08-13 02:01:16 +0900

    20210812_Chap05

---
+++ src/Chap05/RecurX1.java
@@ -0,0 +1,12 @@
+package Chap05;
+
+public class RecurX1 {
+    // 꼬리 재귀를 제거
+    static void recur(int n) {
+        while (n > 0) {
+            recur(n - 1);
+            System.out.println(n);
+            n = n - 2;
+        }
+    }
+}
